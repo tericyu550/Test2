fd6b548803ad62fe480b63ab2aa7da3a480c4c65
Lin_black <tericyu550@gmail.com>
2024-05-11 16:35:19 +0800

    frist commit

---
+++ Assets/Scipt/NPC_AI.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+public class NPC_AI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    NavMeshAgent npcNavi;
+    public GameObject TargetObj;
+    public Animator NPC01_ani;
+    public int DetectDistance = 10;
+    public float AttkDistance = .9f;
+    public float NPC_HP = 100f;
+    static public bool NPC_isAtk = false;
+    public float NPC_AtkTime;
+
+
+    void Start()
+    {
+
+        npcNavi = GetComponent<NavMeshAgent>();
+        NPC01_ani = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(transform.position, TargetObj.transform.position);
+
+        float NPC_Speed = npcNavi.velocity.magnitude;   // 取得NPC的速度. 
+       
+        if (dist < DetectDistance) // 偵測NPC和玩家之間的距離, 如在所設定的距離內, 開始追蹤
+        {
+            npcNavi.destination = TargetObj.transform.position;
+            NPC01_ani.SetFloat("Blend", 1);
+        }
+        else // 沒有的話, 則追蹤自己 或也可以改成回到NPC原先的位置. 
+        {
+            npcNavi.destination = transform.position;
+            NPC01_ani.SetFloat("Blend", 0);
+        }
+        if (dist < AttkDistance)
+        {
+            NPC01_ani.SetBool("NPC_ATK", true);
+        }
+        else
+        {
+            NPC01_ani.SetBool("NPC_ATK", false);
+        }
+        if (NPC_HP <= 0)
+        {
+            NPC01_ani.SetBool("NPC_Die", true);
+            Destroy(gameObject, 5);
+        }
+        NPC_AtkTime = NPC01_ani.GetFloat("NPC_AtkTtime");
+        NPC_isAtk = NPC_AtkTime > 0.002 ? true : false;
+    }
+
+
+    private void OnTriggerEnter(Collider Hit)
+    {
+        if (Hit.gameObject.tag == "weapon" && NPC_HP > 0&& playerC.Player_isAttk==true)
+        {
+            NPC_HP -= 50;
+        }
+    }
+
+    //NavMeshAgent NPCNav;
+    //public GameObject TargetObj;
+    //void Start()
+    //{
+    //    NPCNav = GetComponent<NavMeshAgent>();
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    NPCNav.destination = TargetObj.transform.position;
+    //}
+}
---
+++ Assets/Scipt/playerC.cs
@@ -0,0 +1,142 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class playerC : MonoBehaviour
+{
+    public CharacterController controller;
+    public float moveSpeed = 5f;
+    public float turnSpeed = 200f;
+    public float jumpForce = 5f;
+    public float mtfk;
+ //  private bool isJumping = false;
+  //  private bool groundedPlayer = false;
+    private Rigidbody rb;
+    private float moveInput;
+    public Animator anim;
+    public GameObject knife;
+    public float gravity = 9.8f;
+    public float PlayHp = 100f;
+    public Image PlayHpIMG;
+    public float PlayerAtkTime;
+    static public bool Player_isAttk = false;
+
+    private Vector3 moveDirection;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();      
+    }
+    private void Update()
+    {
+        if (controller.isGrounded)
+        {
+            float tunX = Input.GetAxis("Horizontal");
+            float moveZ = Input.GetAxis("Vertical");
+            moveSpeed = Input.GetKey(KeyCode.LeftShift) ? 6 : 2; //當按下左Shift鍵時，verticalInput的值為3，否則為1  
+            //Debug.Log(moveSpeed);          
+            anim.SetFloat("Blend", Mathf.Abs(moveZ * moveSpeed));
+           // PlayHpIMG.fillAmount = PlayHp * 0.1f;
+            // 計算移動方向
+           // moveDirection = new Vector3(0f, 0f, moveZ);
+            moveDirection = transform.TransformDirection(new Vector3(0f, 0f, moveZ)* moveSpeed);
+            //moveDirection *= moveSpeed;
+            //處理角色轉向
+            transform.Rotate(Vector3.up * tunX * turnSpeed * Time.deltaTime); // 
+                                                                              // Move forward and backward
+            moveInput = Input.GetAxis("Vertical");
+            // 處理重力
+
+
+            //if (Input.GetKey(KeyCode.LeftShift)&& Input.GetAxisRaw("Vertical")!=0 )
+            //{
+            //    transform.Translate(Vector3.forward * moveInput * moveSpeed * Time.deltaTime * 2f);
+            //    anim.SetFloat("Blend", 2);
+            //}
+            //else
+            //{
+            //    transform.Translate(Vector3.forward * moveInput * moveSpeed * Time.deltaTime);
+            //    anim.SetFloat("Blend", moveInput);
+            //}
+
+            //// Turn left and right
+            //float turnInput = Input.GetAxis("Horizontal");
+
+            // transform.Rotate(Vector3.up * turnInput * turnSpeed * Time.deltaTime);
+
+
+            // Jump
+            if (Input.GetButton("Jump"))
+            {
+                // rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                anim.SetTrigger("player_jump");
+                moveDirection.y = jumpForce;
+
+                // anim.SetTrigger("player_jp");
+            }
+
+           
+        }
+        PlayerAtkTime = anim.GetFloat("playerAttacktime");
+        Player_isAttk = PlayerAtkTime > 0.002f ? true : false;
+        moveDirection.y -= gravity * Time.deltaTime;
+            // 移動角色
+        controller.Move(moveDirection * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetBool("Player_attack", true);
+        }
+        else
+            anim.SetBool("Player_attack", false);
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Reset jump state when touching the ground
+        //if (collision.gameObject.CompareTag("Ground"))
+        //{
+        //    isJumping = false;
+        //}
+    }
+    void playerAttack()
+    {
+       
+        StartCoroutine("attack");
+
+    }
+    IEnumerator attack()
+    {
+        
+        yield return new WaitForSeconds(2f);
+    }
+    
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     print("OnTriggerEnter");
+    //     if(other.CompareTag("COIN"))
+    //     {
+    //         Destroy(other.gameObject);
+    //     }
+    // }
+    //取武器碰撞偵測的function 
+    private void OnTriggerEnter(Collider other)
+    {
+       // print("OnTriggerEnter"); //當物件進入觸發區時，印出OnTriggerEnter
+        // if (other.CompareTag("weapon"))
+        // {
+        //    // playerPickWeapon = true;
+        //     sword.SetActive(true); 
+        //     Destroy(other.gameObject); // 刪除進入觸發區的物件
+        // }
+        if(other.CompareTag("COIN"))
+        {
+            // playerPickWeapon = true;
+            knife.SetActive(true);
+            Destroy(other.gameObject);
+        }
+        //if (other.gameObject.tag == "Enemy" && NPC_AI.NPC_isAtk == true)
+        //{ 
+        //}
+     
+    }
+}
